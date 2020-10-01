9e54520a675f74f0f918c2892f04d73b1bbaf4a8
doniiawan <doniiawan@gmail.com>
2020-10-01 12:24:17 +0700

    refactor(router) : delete unused user jwt claims

--- routes/api/v1/user/index.js
+++ routes/api/v1/user/index.js
@@ -3,15 +3,12 @@ var router = express.Router()
 
 router.route('/')
   .get(getRouteHandler)
-  .post(postRouteHandler);
 
 function getRouteHandler(req, res) {
-  //handle GET route here
-  res.status(200).json([{ error: 'SYAUQI' }, { error: 'SYAUQI' }, { error: 'SYAUQI' }, { error: 'SYAUQI' }, { error: 'SYAUQI' }, { error: 'SYAUQI' }])
-}
-
-function postRouteHandler(req, res) {
-  //handle POST route here
+  let payload = res.locals.authuser
+  delete payload.iat
+  delete payload.exp
+  res.status(200).json(payload)
 }
 
 module.exports = router;
